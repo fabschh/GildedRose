75a64a7925680e0ea1464fab579e28d7b89067cd
fabschh <130654495+fabschh@users.noreply.github.com>
2023-05-14 11:27:12 +0200

    Add files via upload

--- GildedRose.cpp
+++ GildedRose.cpp
@@ -2,9 +2,13 @@
 
 #define LIMIT_INCREASE_WORTH_DOUBLE 10
 #define LIMIT_INCREASE_WORTH_TRIPLE 5
-#define SET_WORTH_ZERO 0
+#define MAX_WORTH 50
+#define MIN_WORTH 0
+#define MIN_DAYS 0
 
-GildedRose::GildedRose(vector<Item *> &items) : item_list(items) {}
+GildedRose::GildedRose(vector<Item*> &items) : item_list(items) {
+
+}
 
 GildedRose::~GildedRose() {
     for (auto &item: item_list) {
@@ -15,92 +19,115 @@ GildedRose::~GildedRose() {
 const ItemType is_backstage_pass = EXPIRING;
 const ItemType is_sulfuras = LEGENDARY;
 const ItemType is_aged_brie = MATURING;
+const ItemType is_normal = NORMAL;
 
 void GildedRose::decrement_worth_item(Item *item_pointer) {
-    item_pointer->worth_item--;
+    if(item_pointer->worth_item > MIN_WORTH){
+        item_pointer->worth_item--;
+    }
 }
 
 void GildedRose::increment_worth_item(Item *item_pointer) {
-    item_pointer->worth_item++;
+    if(item_pointer->worth_item < MAX_WORTH){
+        item_pointer->worth_item++;
+    }
 }
 
-bool GildedRose::worth_higher_min(Item *item_pointer) {
-    return item_pointer->worth_item > 0;
-}
-
-bool GildedRose::worth_lower_max(Item *item_pointer) {
-    return item_pointer->worth_item < 50;
-}
-
-bool GildedRose::is_worthy_sulfuras(Item *item_pointer) {
-    if (worth_higher_min(item_pointer) && item_pointer->typeOf() != is_sulfuras) {
+bool GildedRose::is_expired(Item *item_pointer) {
+    if (item_pointer->expires_in_days < MIN_DAYS) {
         return true;
-    } else {
+    }
+    else {
         return false;
     }
 }
 
-bool GildedRose::is_expired(Item *item_pointer){
-    if(item_pointer->expires_in_days < 0){
-        return true;
-    }
-    else{
-        return false;
-    }
-}
-
-void GildedRose::decrement_expires_in_days(Item* item_pointer){
+void GildedRose::decrement_expires_in_days(Item *item_pointer) {
     item_pointer->expires_in_days--;
 }
 
-void GildedRose::handle_expired_item(Item* item_pointer){
+void GildedRose::adapt_worth_backstage_pass(Item* item_pointer){
+    increment_worth_item(item_pointer);
 
-        if (item_pointer->typeOf() != is_aged_brie) {
-            if (item_pointer->typeOf() != is_backstage_pass) {
-                if (is_worthy_sulfuras(item_pointer)) {
-                    decrement_worth_item(item_pointer);
-                }
-            }
-            else{
-                item_pointer->worth_item = SET_WORTH_ZERO;
-            }
-        }
-        else if (worth_lower_max(item_pointer)){
-            increment_worth_item(item_pointer);
-        }
+    if (item_pointer->expires_in_days <= LIMIT_INCREASE_WORTH_DOUBLE) {
+                increment_worth_item(item_pointer);
+    }
+
+    if (item_pointer->expires_in_days <= LIMIT_INCREASE_WORTH_TRIPLE) {
+                increment_worth_item(item_pointer);
+    }
 }
 
-void GildedRose::update_item(Item* this_item) {
-    if (this_item->typeOf() != is_aged_brie && this_item->typeOf() != is_backstage_pass) {
-        if (worth_higher_min(this_item) && this_item->typeOf() != is_sulfuras) {
-            decrement_worth_item(this_item);
-        }
+void GildedRose::update_normal_item(Item* item_pointer){
+    decrement_worth_item(item_pointer);
+
+    decrement_expires_in_days(item_pointer);
+
+    if(is_expired(item_pointer)){
+        decrement_worth_item(item_pointer);
     }
-    else if (worth_lower_max(this_item)){
-            increment_worth_item(this_item);
+}
 
-            if (this_item->typeOf() == is_backstage_pass && worth_lower_max(this_item)) {
-                if (this_item->expires_in_days <= LIMIT_INCREASE_WORTH_DOUBLE) {
-                    increment_worth_item(this_item);
-                }
+void GildedRose::update_backstage_pass(Item* item_pointer){
+    adapt_worth_backstage_pass(item_pointer);
+    decrement_expires_in_days(item_pointer);
 
-                if (this_item->expires_in_days <= LIMIT_INCREASE_WORTH_TRIPLE){
-                    increment_worth_item(this_item);
-                }
-            }
+    if(is_expired(item_pointer)){
+        item_pointer->worth_item = MIN_WORTH;
     }
+}
 
-    if (this_item->typeOf() != is_sulfuras) {
-        decrement_expires_in_days(this_item);
+void GildedRose::update_aged_brie(Item* item_pointer){
+    increment_worth_item(item_pointer);
+
+    decrement_expires_in_days(item_pointer);
+    if(is_expired(item_pointer)){
+        increment_worth_item(item_pointer);
     }
+}
 
-    if(is_expired(this_item)){
-            handle_expired_item(this_item);
+void GildedRose::update_sulfuras(Item* item_pointer){
+
+}
+
+void GildedRose::update_conjured(Item* item_pointer){
+    decrement_worth_item(item_pointer);
+
+    decrement_expires_in_days(item_pointer);
+
+    if(is_expired(item_pointer)){
+        decrement_worth_item(item_pointer);
+    }
+    /*decrement_worth_item(item_pointer);    10/10 passed
+    decrement_worth_item(item_pointer);
+
+     decrement_expires_in_days(item_pointer);
+
+    if(is_expired(item_pointer)){
+        decrement_worth_item(item_pointer);
+    }*/
+}
+
+void GildedRose::update_item(Item *this_item) {
+    if(this_item->typeOf() == is_normal){
+        update_normal_item(this_item);
+    }
+    if(this_item->typeOf() == is_backstage_pass){
+        update_backstage_pass(this_item);
+    }
+    if(this_item->typeOf() == is_aged_brie){
+        update_aged_brie(this_item);
+    }
+    if(this_item->typeOf() == is_sulfuras){
+        update_sulfuras(this_item);
+    }
+    if(this_item->typeOf() == CONJURED){
+        update_conjured(this_item);
     }
 }
 
 void GildedRose::update_all_items() {
-    for (Item* this_item : item_list) {
+    for (Item *this_item : item_list) {
         update_item(this_item);
     }
 }
--- GildedRose.h
+++ GildedRose.h
@@ -7,19 +7,22 @@ using namespace std;
 class GildedRose
 {
 public:
-    vector<Item*> & item_list;
-    GildedRose(vector<Item*> & items);
+    vector<Item*> &item_list;
+    GildedRose(vector<Item*> &items);
     Item* createItem(string,int,int) const;
     void update_all_items();
     void update_item(Item* this_item);
     void decrement_worth_item(Item *item_pointer);
     void increment_worth_item(Item *item_pointer);
-    bool worth_higher_min(Item *item_pointer);
-    bool worth_lower_max(Item *item_pointer);
-    bool is_worthy_sulfuras(Item *item_pointer);
     bool is_expired(Item *item_pointer);
     void decrement_expires_in_days(Item* item_pointer);
-    void handle_expired_item(Item* item_pointer);
+
+    void update_normal_item(Item* item_pointer);
+    void update_backstage_pass(Item* item_pointer);
+    void adapt_worth_backstage_pass(Item* item_pointer);
+    void update_aged_brie(Item* item_pointer);
+    void update_sulfuras(Item* item_pointer);
+    void update_conjured(Item* item_pointer);
     virtual ~GildedRose();
 };
 
